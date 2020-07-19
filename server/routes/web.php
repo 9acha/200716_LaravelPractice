0883d59e1ad9bf16ee6b4b66a72e18854196bed6
Yuhei Matsuura <yuhei.m848@gmail.com>
2020-07-20 08:29:59 +0900

    4.3の途中

--- server/routes/web.php
+++ server/routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index')->middleware('hello');
-// Route::post('hello', 'HelloController@post');
+Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
